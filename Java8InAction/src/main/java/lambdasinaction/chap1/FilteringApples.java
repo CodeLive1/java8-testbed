8a135e9020c0beedae4ca9d3515701879d5342b2
rajag <VivekRG@yahoo.com>
2018-06-13 19:15:28 -0500

    Testing pipeline

--- Java8InAction/src/main/java/lambdasinaction/chap1/FilteringApples.java
+++ Java8InAction/src/main/java/lambdasinaction/chap1/FilteringApples.java
@@ -7,7 +7,7 @@ public class FilteringApples{
 
 //  Adding comments to check branch changes
     public static void main(String ... args){
-        System.out.println("Testing FilteringApples");
+        System.out.println("Testing FilteringApples Pipeline");
 
         List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
                                               new Apple(155, "green"),
